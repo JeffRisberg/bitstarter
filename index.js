c7d2a8f93ce4ff2441cedc4584334500d0175b0a
Ubuntu <ubuntu@ip-172-31-33-92.us-west-2.compute.internal>
2014-08-11 02:10:30 +0000

    Added index.html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+var fs = require('fs');
 var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var buffer = fs.fileReadSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello Jeff!')
+  response.send(buffer.toString());
 })
 
 app.listen(app.get('port'), function() {
