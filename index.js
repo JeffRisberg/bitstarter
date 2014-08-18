4514679cc56c68623915c9d2491593b7b2973782
Jeff Risberg <jrisberg@aol.com>
2014-08-18 06:31:55 +0000

    updated port number, updated example to use Bootstrap 3.2.0

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Node app is running at localhost:" + port);
 })
